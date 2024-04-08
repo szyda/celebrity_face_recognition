6b2b468cf9500b437088609c6f6edd63b2ce352b
szyda <szydasylwia@protonmail.com>
2024-04-08 18:07:08 +0200

    refactor and add method regarding data processing

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,19 +1,18 @@
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import face_recognition
+import os, cv2
+from datetime import datetime
 
 class DataPreprocessing:
-    def __init__(self, data_directory, image_size=(244, 244), batch_size=32):
+    def __init__(self, data_directory="./data/", image_size=(224, 224), batch_size=32):
         self.data_directory = data_directory
         self.image_size = image_size
         self.batch_size = batch_size
+        self.num_classes, self.class_indices = self.get_class_data()
 
-    """
-    Generator danych z opcjonalną augmentacją dla danych treningowych. 
-    Augumentacja to proces tworzenia zmodyfikowanego obrazu (np. poprzez odwrócenie, 
-    czy odbicie lustrzane). Doda nam to różnorodności do zbioru danych. 
-    """
     def initialize_datagen(self, augment=False):
         if augment:
-            data = ImageDataGenerator(
+            data_gen = ImageDataGenerator(
                 rescale=1./255,
                 rotation_range=20,
                 width_shift_range=0.2,
@@ -21,22 +20,18 @@ class DataPreprocessing:
                 shear_range=0.2,
                 horizontal_flip=True,
                 fill_mode='nearest',
-                validation_split=0.2
+                validation_split=0.2  # 80% training 20% validation
             )
         else:
-            data = ImageDataGenerator(
+            data_gen = ImageDataGenerator(
                 rescale=1./255,
                 validation_split=0.2
             )
 
-        return data
+        return data_gen
 
-    """
-    Funkcja wczytuje dane dla wybranego podzbioru: training/validation
-    """
     def load_data(self, subset):
-        datagen = self.initialize_datagen(augment=(subset == 'training')) # augumentujemy tylko zbiory treningowe (najbezpieczniej imo)
-
+        datagen = self.initialize_datagen(augment=(subset == 'training'))
         return datagen.flow_from_directory(
             self.data_directory,
             target_size=self.image_size,
@@ -45,12 +40,57 @@ class DataPreprocessing:
             subset=subset
         )
 
-    """
-    Pomocnicze funkcje / helpers, które ułatwią dostęp do danych treningowych/walidacyjnych
-    """
+    def get_class_data(self):
+        classes = [d for d in os.listdir(self.data_directory) if os.path.isdir(os.path.join(self.data_directory, d))]
+        classes.sort()
+        class_indices = {cls: idx for idx, cls in enumerate(classes)}
+        return len(classes), class_indices
 
     def get_train_data(self):
         return self.load_data('training')
 
     def get_validation_data(self):
         return self.load_data('validation')
+
+    def preprocess_image(self, image_path):
+        """Preprocess an image to make it model-ready.
+
+        Args:
+            image_path (str): Path to the image file.
+
+        Returns:
+            numpy.ndarray: The preprocessed image data array.
+        """
+        img = load_img(image_path, target_size=self.image_size)
+        img_array = img_to_array(img)
+        img_array = np.expand_dims(img_array, axis=0)  # Reshape to (1, height, width, channels)
+        img_array /= 255.0  # Normalize the image
+        return img_array
+
+    def crop_faces(self):
+        for subdir, dirs, files in os.walk(self.data_directory):
+            output_folder = os.path.join(subdir, "processed")
+            os.makedirs(output_folder, exist_ok=True)
+
+            for file in files:
+                image_path = os.path.join(subdir, file)
+                image = cv2.imread(image_path)
+                if image is None:
+                    print(f"Failed to load image. Skipping: {image_path}")
+                    continue
+
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Convert to RGB for face_recognition
+                face = self.detect_faces(image)
+                if face:
+                    top, right, bottom, left = face
+                    face_image = image[top:bottom, left:right]
+                    face_image = cv2.cvtColor(face_image, cv2.COLOR_RGB2BGR)  # Convert to BGR for OpenCV
+                    timestamp = datetime.now().strftime("%Y%m%d%H%M%S%f")
+                    face_file_path = os.path.join(output_folder, f"face_{timestamp}.jpg")
+                    cv2.imwrite(face_file_path, face_image)
+
+    def detect_faces(self, image):
+        face_locations = face_recognition.face_locations(image)
+        if face_locations:
+            return face_locations[0]
+        return None
